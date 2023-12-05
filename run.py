e591947a7220dac2e82ae418bc7d5339dea29cbb
WIDIANNE_DALVALLEN <widiannedalvallen@gmail.com>
2023-12-05 22:34:50 +0100

    fix:lol added words.py again

--- run.py
+++ run.py
@@ -6,14 +6,6 @@ data_str = input("enter data here:\n")
 print("Let's play Hangman [] ")
 print("-------------------------------------------")
 
-wordDictionary = ['advance', 'alcohol', 'aperture', 'arrangement', 'artist', 'art', 'autonomy',  
-'bubble', 'buffoon', 'butch', 'caricature', 'celebration', 'chaos', 'coke', 'cool', 'consequently', 
-  'conundrum', 'corpse', 'dare', 'death', 'die', 'discomfit', 'dogma', 'enthuzimuzzy', 'escape', 
-  'ethereal', 'femme', 'feminist', 'feet', 'fine', 'flesh', 'flower', 'fun', 'garbage', 'gay', 
-  'gaze', 'glory', 'grounds', 'headline', 'house', 'interface', 'joke', 'lamp', 'leather',  'luck', 
-  'metaphor', 'microphone', 'money', 'morbs', 'mystery', 'painter', 'pain', 'paradox', 'party', 
-  'poetry', 'possibility', 'protest', 'queer', 'reality', 'soiree', 'soup', 'spy', 'suffer', 
-  'texture', 'wrist' ] 
 
 # Choose a random word
 randomWord = random.choice(wordDictionary)
---
+++ words.py
@@ -0,0 +1,8 @@
+wordDictionary = ['advance', 'alcohol', 'aperture', 'arrangement', 'artist', 'art', 'autonomy',  
+'bubble', 'buffoon', 'butch', 'caricature', 'celebration', 'chaos', 'coke', 'cool', 'consequently', 
+  'conundrum', 'corpse', 'dare', 'death', 'die', 'discomfit', 'dogma', 'enthuzimuzzy', 'escape', 
+  'ethereal', 'femme', 'feminist', 'feet', 'fine', 'flesh', 'flower', 'fun', 'garbage', 'gay', 
+  'gaze', 'glory', 'grounds', 'headline', 'house', 'interface', 'joke', 'lamp', 'leather',  'luck', 
+  'metaphor', 'microphone', 'money', 'morbs', 'mystery', 'painter', 'pain', 'paradox', 'party', 
+  'poetry', 'possibility', 'protest', 'queer', 'reality', 'soiree', 'soup', 'spy', 'suffer', 
+  'texture', 'wrist' ] 
