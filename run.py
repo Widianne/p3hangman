951f702c6661181d3662a4b25c84eaf61571dc20
WIDIANNE_DALVALLEN <widiannedalvallen@gmail.com>
2023-12-05 12:57:25 +0100

    add:welcome text

--- run.py
+++ run.py
@@ -3,3 +3,54 @@ from words import wordDictionary
 
 print ("Let's play Hangman")
 print ("-------------------------------------------")
+
+###choose random word
+randomWord = random.choice(wordDictionary)
+
+fix x in randomWord:
+    print("_", end= " ")
+    
+def print_hangman(wrong):
+     if(wrong == 0):
+    print("\n+---+")
+    print("    |")
+    print("    |")
+    print("    |")
+    print("   ===")
+  elif(wrong == 1): 
+    print("\n+---+")
+    print("O   |")
+    print("    |")
+    print("    |")
+    print("   ===")
+  elif(wrong == 2):
+    print("\n+---+")
+    print("O   |")
+    print("|   |")
+    print("    |")
+    print("   ===")
+  elif(wrong == 3):
+    print("\n+---+")
+    print(" O  |")
+    print("/|  |")
+    print("    |")
+    print("   ===")
+  elif(wrong == 4):
+    print("\n+---+")
+    print(" O  |")
+    print("/|\ |")
+    print("    |")
+    print("   ===")
+  elif(wrong == 5):
+    print("\n+---+")
+    print(" O  |")
+    print("/|\ |")
+    print("/   |")
+    print("   ===")
+  elif(wrong == 6):
+    print("\n+---+")
+    print(" O   |")
+    print("/|\  |")
+    print("/ \  |")
+    print("    ===")
+    
