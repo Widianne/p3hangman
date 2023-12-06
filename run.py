f4f94ff534198452fa6740f32979bdca90ac970a
WIDIANNE_DALVALLEN <widiannedalvallen@gmail.com>
2023-12-06 19:41:10 +0100

    fix:cleaned up the lill code

--- run.py
+++ run.py
@@ -2,17 +2,17 @@
 imports from necessary libraries
 """
 import random
+import colorama
 from words import wordDictionary
 
-import colorama 
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 colorama.init()
 
 """
 the game title 
 """
 
-print("✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ")
+print(Fore.RED + "✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ")
 print('''                      
 ████████╗██╗  ██╗███████╗    ██╗  ██╗ █████╗ ███╗   ██╗ ██████╗ ███╗   ███╗ █████╗ ███╗   ██╗
 ╚══██╔══╝██║  ██║██╔════╝    ██║  ██║██╔══██╗████╗  ██║██╔════╝ ████╗ ████║██╔══██╗████╗  ██║
@@ -22,11 +22,11 @@ print('''
    ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝                    
       ''')
 
-"""
-the rules of the game
-"""
 
 def print_rules():
+    """
+    Prints the rules
+    """
     print("✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ \n")
     print("Let's play! Here are the rules:")
     print("1. The computer selects a random word from a secret list of words.")
@@ -37,23 +37,109 @@ def print_rules():
     print("6. You win if you guess the word before reaching the maximum incorrect guesses.")
     print("7. You lose if you run out of guesses and the hangman is fully drawn.\n")
     print("✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ ✧.* ˚ ✦ \n")
+    print(Style.RESET_ALL)
 # call the function to print the rules
-print_rules()
+
 
 """
-the acctual game
+the actual game
 """
-# chooses a random word
-randomWord = random.choice(wordDictionary)
 
-for letter in randomWord:
-    print("_", end=" ")
+
+def get_word():
+    """
+    Gets a random word
+    """
+    return random.choice(wordDictionary)
+
+
+def print_word(guessed_letters, random_word):
+    """
+    Prints the word
+    """
+    counter = 0
+    right_letters = 0
+    for char in random_word:
+        if char in guessed_letters:
+            print(random_word[counter], end=" ")
+            right_letters += 1
+        else:
+            print(" ", end=" ")
+        counter += 1
+    return right_letters
+
+
+def print_lines(random_word):
+    """
+    Prints lines for word
+    """
+    print("\r")
+    for char in random_word:
+        print("\u203E", end=" ")
+
+
+def play_game():
+    """
+    Gets a random word and plays the core game loop
+    """
+    random_word = get_word()
+    print(random_word)
+    for letter in random_word:
+        print("_", end=" ")
+
+    length_of_word_to_guess = len(random_word)
+    amount_of_times_wrong = 0
+    current_guess_index = 0
+    current_letters_guessed = []
+    current_letters_right = 0
+
+    while amount_of_times_wrong != 6 and current_letters_right != length_of_word_to_guess:
+        print("\nGuessed letters: ")
+        for letter in current_letters_guessed:
+            print(letter, end=" ")
+        
+
+        # Prompt user for input
+        letter_guessed = input("\nGuess a lill letter: ")
+
+        if not letter_guessed.isalpha() or len(letter_guessed) > 1 or len(letter_guessed) == 0:
+            print('Please only guess one letter at a time!')
+            continue
+
+        if current_guess_index < len(random_word) and letter_guessed == random_word[current_guess_index]:
+            current_letters_guessed.append(letter_guessed)
+            current_guess_index += 1
+            current_letters_right = print_word(
+                current_letters_guessed, random_word)
+            print_lines(random_word)
+        else:
+            amount_of_times_wrong += 1
+            print_hangman(amount_of_times_wrong)
+            current_letters_guessed.append(letter_guessed)
+            current_letters_right = print_word(
+                current_letters_guessed, random_word)
+            print_lines(random_word)
+
+    print("""
+                                                  (`-')  _ <-. (`-')   (`-')  _                     (`-') (`-')  _   (`-')  
+   <-.        .->      <-.           .->    (OO ).-/    \(OO )_  ( OO).-/         .->        _(OO ) ( OO).-/<-.(OO )  
+ ,--. )  (`-')----.  ,--. )       ,---(`-') / ,---.  ,--./  ,-.)(,------.    (`-')----. ,--.(_/,-.\(,------.,------,) 
+ |  (`-')( OO).-.  ' |  (`-')    '  .-(OO ) | \ /`.\ |   `.'   | |  .---'    ( OO).-.  '\   \ / (_/ |  .---'|   /`. ' 
+ |  |OO )( _) | |  | |  |OO )    |  | .-, \ '-'|_.' ||  |'.'|  |(|  '--.     ( _) | |  | \   /   / (|  '--. |  |_.' | 
+(|  '__ | \|  |)|  |(|  '__ |    |  | '.(_/(|  .-.  ||  |   |  | |  .--'      \|  |)|  |_ \     /_) |  .--' |  .   .' 
+ |     |'  '  '-'  ' |     |'    |  '-'  |  |  | |  ||  |   |  | |  `---.      '  '-'  '\-'\   /    |  `---.|  |\  \  
+ `-----'    `-----'  `-----'      `-----'   `--' `--'`--'   `--' `------'       `-----'     `-'     `------'`--' '--' 
+      """)
     
-"""
-hang man img code that builds the hanged man with every wrong guess, 6 trys until game over
-"""
+    restart = input('press Y to restart')
+    if restart.lower() == 'y':
+        play_game()
+
 
 def print_hangman(wrong):
+    """
+    Prints hangman graphic
+    """
     if wrong == 0:
         print("\n+---+")
         print("    |")
@@ -97,76 +183,13 @@ def print_hangman(wrong):
         print("/ \\  |")
         print("    ===")
 
-"""
-this function takes the list of guessed letters and prints the characters of a randomly chosen word, 
-revealing guessed letters and hiding the unguessed ones. The function returns the count of 
-correctly guessed letters.
-"""
-def printWord(guessedLetters):
-    counter = 0
-    rightLetters = 0
-    for char in randomWord:
-        if char in guessedLetters:
-            print(randomWord[counter], end=" ")
-            rightLetters += 1
-        else:
-            print(" ", end=" ")
-        counter += 1
-    return rightLetters
 
-"""
-displays a visual representation of the word, where each letter is represented by a horizontal line
-"""
-def printlines():
-    print("\r")
-    for char in randomWord:
-        print("\u203E", end=" ")
-
-"""
-tracks the length of the word, incorrect guesses, current guess index, guessed letters, 
-and the count of correct letters guessed
-"""
-length_of_word_to_guess = len(randomWord)
-amount_of_times_wrong = 0
-current_guess_index = 0
-current_letters_guessed = []
-current_letters_right = 0
-
-"""
-prints the guessed letters, prompts the user to guess, stores input in the variable letterGuessed. 
-loop continues until either the player makes 6 incorrect guesses or correctly guesses all letters
-"""
-while amount_of_times_wrong != 6 and current_letters_right != length_of_word_to_guess:
-    print("\nGuessed letters: ")
-    for letter in current_letters_guessed:
-        print(letter, end=" ")
-
-    # Prompt user for input
-    letterGuessed = input("\nGuess a lill letter: ")
-
-    # Check if the letter is correct
-    if current_guess_index < len(randomWord) and letterGuessed == randomWord[current_guess_index]:
-        current_letters_guessed.append(letterGuessed)
-        current_guess_index += 1
-        current_letters_right = printWord(current_letters_guessed)
-        printlines()
-    else:
-        amount_of_times_wrong += 1
-        print_hangman(amount_of_times_wrong)
-        current_letters_right = printWord(current_letters_guessed)
-        printlines()
-
-print('''
-                                                  (`-')  _ <-. (`-')   (`-')  _                     (`-') (`-')  _   (`-')  
-   <-.        .->      <-.           .->    (OO ).-/    \(OO )_  ( OO).-/         .->        _(OO ) ( OO).-/<-.(OO )  
- ,--. )  (`-')----.  ,--. )       ,---(`-') / ,---.  ,--./  ,-.)(,------.    (`-')----. ,--.(_/,-.\(,------.,------,) 
- |  (`-')( OO).-.  ' |  (`-')    '  .-(OO ) | \ /`.\ |   `.'   | |  .---'    ( OO).-.  '\   \ / (_/ |  .---'|   /`. ' 
- |  |OO )( _) | |  | |  |OO )    |  | .-, \ '-'|_.' ||  |'.'|  |(|  '--.     ( _) | |  | \   /   / (|  '--. |  |_.' | 
-(|  '__ | \|  |)|  |(|  '__ |    |  | '.(_/(|  .-.  ||  |   |  | |  .--'      \|  |)|  |_ \     /_) |  .--' |  .   .' 
- |     |'  '  '-'  ' |     |'    |  '-'  |  |  | |  ||  |   |  | |  `---.      '  '-'  '\-'\   /    |  `---.|  |\  \  
- `-----'    `-----'  `-----'      `-----'   `--' `--'`--'   `--' `------'       `-----'     `-'     `------'`--' '--' 
-      ''')
+def main():
+    """
+    initial game build
+    """
+    print_rules()
+    play_game()
 
 
-            
-
+main()
